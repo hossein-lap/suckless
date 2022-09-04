cdf2c9202704b6126124b0d8b8bf41ed3341f669
hossein <hosteam01@gmail.com>
2022-09-04 23:47:55 +0430

    tabbed: applied clientnumber patch

--- tabbed/config.def.h
+++ tabbed/config.def.h
@@ -2,10 +2,10 @@
 
 /* appearance */
 static const char font[]        = "Source Code Pro:size=18";
-static const char* normbgcolor  = "#1E1F29";
+static const char* normbgcolor  = "#282A36";
 static const char* normfgcolor  = "#F8F8EF";
 static const char* selbgcolor   = "#11121D";
-static const char* selfgcolor   = "#E45678";
+static const char* selfgcolor   = "#BD93F9";
 static const char* urgfgcolor   = "#111111";
 static const char* urgbgcolor   = "#ff0000";
 static const char before[]      = "<";
