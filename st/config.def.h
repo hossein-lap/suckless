24ba3d8dcb056daec840f18a7b36722c3fd08d71
hossein <hosteam01@gmail.com>
2022-09-15 02:50:49 +0430

    st: applied [newterm] patch

--- st/config.def.h
+++ st/config.def.h
@@ -211,6 +211,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_s,           changealpha,    {.f = -0.05} },
 	{ TERMMOD,              XK_a,           changealpha,    {.f = +0.05} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
