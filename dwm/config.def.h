8f523e15f2abdad6cb71331c1439090d959c744e
hossein <hosteam01@gmail.com>
2022-10-08 18:48:17 +0330

    sat some window rules
        - Firefox on tag[4]
        - Telegram on tag[5]
    
    defined some macros
    
        - Path of scripts
    
    defined some strings
    
        - Default apps like terminal and browser

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,41 +2,44 @@
 
 /* vars */
 static const char term[]		= "st";
+static const char fileman[]		= "ranger";
+static const char browser[]		= "firefox";
+static const char muspley[]		= "cmus";
+//static const char rssread[]		= "newsboat";
+
 static const char ndterm[]		= "alacritty";
 static const char bdterm[]		= "lxterminal";
 
 #define SCRIPTSPATH "~/.local/dev/hossein-lap/scripts/"
 
+/* colorschemes */
+/*
+ * List if colors:
+ *   - blue:      "#0077ff" - cyan:      "#00aaff"
+ *   - pink:      "#ff77cc" - purple:    "#cc77ff"
+ *   - red:       "#ff0000" - darkpink:  "#e95678"
+ *   - orange:    "#ff7700" - yellow:    "#ffd500"
+ *   - white:     "#ffffff"
+ *   - dracula
+ *   - soalrized-dark: {blue,green,pink,yellow}
+ */
+#include "colors/bg/orange.h"
+
+static const char *colors[][3]	  = {
+        /*                     fg        bg        border   */
+	[SchemeNorm]	= { normal_fg, normal_bg, normal_br },
+	[SchemeSel]	= { focus_fg, focus_bg, focus_br },
+};
+
 /* appearance */
 static const unsigned int borderpx	= 2;	/* border pixel of windows */
 static const unsigned int snap		= 32;	/* snap pixel */
-static const unsigned int gappx		= 18;	/* gap pixel between windows */
+static const unsigned int gappx		= 15;	/* gap pixel between windows */
 static const int showbar		= 1;	/* 0 means no bar */
 static const int topbar			= 1;	/* 0 means bottom bar */
 
 static const char *fonts[]		= { "Fira Code:size=15" };
 static const char dmenufont[]		= { "Fira Code:size=15" };
-
-/* colorschemes */
-/*
- * List if colors:
- *   - blue:      "#0077ff"
- *   - cyan:      "#00aaff"
- *   - darkpink:  "#e95678"
- *   - orange:    "#ff7700"
- *   - pink:      "#ff77cc"
- *   - purple:    "#cc77ff"
- *   - red:       "#ff0000"
- *   - white:     "#ffffff"
- *   - yellow:    "#ffd500"
- */
-#include "colors/dracula.h"
-
-static const char *colors[][3]	  = {
-	/*			fg	bg	border   */
-	[SchemeNorm]	= { normal_fg, normal_bg, normal_br },
-	[SchemeSel]	= { focus_fg, focus_bg, focus_br },
-};
 
 /* tagging */
 //static const char *tags[] = { "sys", "dev", "www", "dir", "vid", "doc", "em", "pre", "tmp" };
@@ -53,10 +56,10 @@ static const Rule rules[] = {
 	 */
 	/* class	instance	title		tags mask	isfloating	monitor */
 //	{ "Gimp",	NULL,	NULL,				0,		1,	-1 },
-//	{ "Firefox",	NULL,	NULL,				1 << 8,		0,	-1 },
-	{ "nsxiv",	NULL,	NULL,				0,		1,	-1 },
-	{ "persepolis",	NULL,	"Persepolis Download Manager",	0,		1,	-1 },
-	{ "Telegram",	NULL,	"Telegram",			0,		1,	-1 },
+	{ "Firefox",	NULL,	NULL,				1 << 8,		0,	-1 },
+	{ "sxiv",	NULL,	NULL,				0,		1,	-1 },
+	//{ "persepolis",	NULL,	"Persepolis Download Manager",	0,		1,	-1 },
+	{ "Telegram",	NULL,	"Telegram",			4,		0,	-1 },
 	{ "sterm",	NULL,	"Term",				0,		1,	-1 },
 	{ "st",		NULL,	"Terminal",			0,		1,	-1 },
 	{ "st",		NULL,	"Note",				0,		1,	-1 },
@@ -71,8 +74,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[M]",      monocle },  /* first entry is default */
 	{ "[]=",      tile },
+	{ "[M]",      monocle },  /* first entry is default */
 	{ "><>",      NULL },   /* no layout function means floating behavior */
 };
 
@@ -89,23 +92,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenupln[]	= { "dmenu_run", "-p", "run:", "-m", dmenumon, "-fn",
-					dmenufont, "-nb", normal_bg, "-nf", normal_fg,
-					"-sb", focus_bg, "-sf", focus_fg, NULL };
-static const char *dmenucmd[]	= { "dmenu_run", "-p", "run:", "-m", dmenumon, "-fn",
-					dmenufont, "-nb", normal_bg, "-nf", normal_fg,
-					"-sb", focus_bg, "-sf", focus_fg,
-					"-c", "-l", "15", "-g", "1", NULL };
+static const char *dmenupln[]	= { "dmenu_run", "-p", "run:", "-m", dmenumon, "-fn", dmenufont,
+	"-bw", "0", "-nb", normal_bg, "-nf", normal_fg, "-sb", focus_bg, "-sf", focus_fg, NULL };
+static const char *dmenucmd[]	= { "dmenu_run", "-p", "run:", "-m", dmenumon, "-fn", dmenufont, "-nb", normal_bg, "-nf", normal_fg,
+					"-sb", focus_bg, "-sf", focus_fg, "-c", "-l", "15", "-g", "1", NULL };
 static const char *termcmd[]	= { term, NULL };
 static const char *ndtmcmd[]	= { ndterm, NULL };
 /* hos custom {{{ */
 
-//static const char *tabbdcmd[]	= { "tabbed", "-c", "-o", normal_bg, "-O", normal_fg,
-					//"-t", focus_bg, "-T", focus_fg, "-r", "2", "st",
-					//"-w", "''", NULL };
-static const char *floatcmd[]	= { "st", "-t", "Terminal", NULL};
+//static const char *tabbdcmd[]	= { "tabbed", "-c", "-r", "2", "st", "-w", "''", NULL };
+static const char *floatcmd[]	= { term, "-t", "Terminal", NULL};
+//static const char *floatcmd[]	= { "st", "-t", "Terminal", NULL};
 static const char *takenote[]	= { "st", "-t", "Note", "bash", "-c", SCRIPTSPATH"note-take.sh", NULL};
 static const char *bidicmd[]	= { bdterm, NULL};
+
+static const char *filecmd[]	= { term, "-t", "Terminal", fileman, NULL};
+//static const char *newsboat[]	= { term, "-t", "Terminal", rssread, NULL};
+static const char *muscmd[]	= { "bash", "-c", SCRIPTSPATH"cmus-tmux.sh", NULL};
+static const char *wwwcmd[]	= { browser, NULL};
 
 static const char *dmshot[]	= { "bash", "-c", SCRIPTSPATH"dm-shot.sh", dmenumon, NULL};
 
@@ -113,14 +117,14 @@ static const char *dmkill[]	= { "bash", "-c", SCRIPTSPATH"dm-kill.sh", dmenumon,
 static const char *dmsrun[]	= { "bash", "-c", SCRIPTSPATH"dm-srun.sh", dmenumon, NULL};
 static const char *dmusbd[]	= { "bash", "-c", SCRIPTSPATH"dm-usb.sh", dmenumon, NULL};
 static const char *scrn_lock[]	= { "slock", NULL};
-//static const char *emclient[]	= { "emacsclient", "-c", "-a", "emacs", NULL};
+static const char *emclient[]	= { "emacsclient", "-c", "-a", "emacs", NULL};
 /* }}}*/
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 /* hos custom {{{ */
-	//{ MODKEY|ShiftMask,		XK_t,		spawn,		{.v = tabbdcmd	} },
 	{ MODKEY|ShiftMask,		XK_t,		spawn,		{.v = ndtmcmd	} },
+	//{ MODKEY|ShiftMask,		XK_Return,	spawn,		{.v = tabbdcmd	} },
 	{ MODKEY|ShiftMask,		XK_Return,	spawn,		{.v = termcmd	} },
 	{ MODKEY,			XK_x,		spawn,		{.v = floatcmd	} },
 	{ MODKEY|ControlMask,		XK_t,		spawn,		{.v = bidicmd	} },
@@ -129,9 +133,14 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_r,		spawn,		{.v = dmsrun	} },
 	{ MODKEY|ShiftMask,		XK_d,		spawn,		{.v = dmusbd	} },
 	{ MODKEY|ShiftMask,		XK_l,		spawn,		{.v = scrn_lock	} },
+
+	{ MODKEY|ShiftMask,		XK_f,		spawn,		{.v = filecmd	} },
+	{ MODKEY|ShiftMask,		XK_w,		spawn,		{.v = wwwcmd	} },
+	{ MODKEY|ShiftMask,		XK_m,		spawn,		{.v = muscmd	} },
+
 	{ MODKEY,			XK_n,		spawn,		{.v = takenote	} },
 	{ MODKEY,			XK_r,		spawn,		{.v = dmenupln } },
-	//{ MODKEY,			XK_e,		spawn,		{.v = emclient	} },
+	{ MODKEY,			XK_e,		spawn,		{.v = emclient	} },
 /* }}}*/
 	{ MODKEY,			XK_p,		spawn,		{.v = dmenucmd } },
 	{ MODKEY,			XK_b,		togglebar,	{0} },
@@ -144,8 +153,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_Return,	zoom,		{0} },
 	{ MODKEY,			XK_Tab,		view,		{0} },
 	{ MODKEY|ShiftMask,		XK_c,		killclient,	{0} },
-	{ MODKEY,			XK_m,		setlayout,	{.v = &layouts[0]} },
-	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[1]} },
+	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
+	{ MODKEY,			XK_m,		setlayout,	{.v = &layouts[1]} },
 	{ MODKEY,			XK_f,		setlayout,	{.v = &layouts[2]} },
 	{ MODKEY,			XK_space,	setlayout,	{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
