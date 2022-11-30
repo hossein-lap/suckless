240608c912de4f6aa4d9437bc1ceab1a953a0602
hossein <hosteam01@gmail.com>
2022-12-01 02:46:23 +0330

    switch to right colors

--- tabbed/colors/dracula.h
+++ tabbed/colors/dracula.h
@@ -1,7 +1,8 @@
-
-static const char* normbgcolor  = "#1E1F29";
+static const char* normbgcolor  = "#282a36";
 static const char* normfgcolor  = "#F8F8EF";
-static const char* selbgcolor   = "#11121D";
-static const char* selfgcolor   = "#BD93F9";
+static const char* selbgcolor   = "#1e1f29";
+static const char* selfgcolor   = "#f8f8f2";
 static const char* urgfgcolor   = "#111111";
 static const char* urgbgcolor   = "#ff0000";
+
+#define DCOL_NORMBG normbgcolor
